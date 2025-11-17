8fcef69eee0e88c7b2ec8ada0525e9a745411609
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2025-11-17 19:19:44 +0200

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+
+function plural(n) {
+  return n !== 1;
+}
+
+// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+
+function plural(n) {
+  return n !== 1;
+}
+
+// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+
+function plural(n) {
+  return n !== 1;
+}
