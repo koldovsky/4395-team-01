fa75e6ed69b3cab48259d029b09f86f633a39a16
Serhii Karpus <49605599+Siren1790@users.noreply.github.com>
2025-11-17 21:42:44 +0000

    lins file JS

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,3 +1,9 @@
+console.log(goals(43, 10, 5)); // 58
+console.log(makeNegative(5)); // -5
+console.log(move(3, 6)); // 15
+console.log(greet("Daniel", "Daniel")); // "Hello boss"
+console.log(litres(3)); // 1
+console.log(lovefunc(1,4)); // true
 
 // Grasshopper - Messi goals function
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
