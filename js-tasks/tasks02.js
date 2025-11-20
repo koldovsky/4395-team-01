b99abb656fd0232dd49e74bb7577f974448b3905
Sofiia Liashenko <mariana.kisla.buba@gmail.com>
2025-11-20 18:44:51 +0200

    added DNA to RNA solution

--- js-tasks/tasks02.js
+++ js-tasks/tasks02.js
@@ -5,4 +5,9 @@ function stringToArray(string){
   let words = string.split(" ");
   return words;
 }
-
+// DNA to RNA Conversion
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+// Liashenko Sofiia Solution
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U");
+}
