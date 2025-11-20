fd5b7556b7f2f73daead33a96aaab682081416cd
Sofiia Liashenko <mariana.kisla.buba@gmail.com>
2025-11-20 18:29:40 +0200

    Create tasks02.js

---
+++ js-tasks/tasks02.js
@@ -0,0 +1,8 @@
+// Convert a string to an array
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+// Liashenko Sofiia Solution
+function stringToArray(string){
+  let words = string.split(" ");
+  return words;
+}
+
