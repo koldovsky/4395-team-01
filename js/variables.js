3cf9a39642fae7e900ec17e9bed33152026dce40
Serhii Karpus <karpus.sergey@gmail.com>
2025-12-04 00:00:39 +0200

    updete date in variables.js

--- js/variables.js
+++ js/variables.js
@@ -2,7 +2,7 @@ const renaissanceTours = [
   {
     id: 1,
     title: "A Journey Through the Renaissance",
-    date: "2023-08-13",
+    date: "AUG 13, 2023",
     time: "12:30 PM – 1:15 PM",
     description: "This tour will take you on a journey through the Renaissance, from the early days of the movement in Italy to its later flowering in Northern Europe. You will see some of the most famous paintings from this period.",
     themeImage: "./img/latest-events/journey-through.webp"
@@ -10,7 +10,7 @@ const renaissanceTours = [
   {
     id: 2,
     title: "The Renaissance and You",
-    date: "AUG 13, 2023",
+    date: "SEP 22, 2023",
     time: "12:30 PM – 3:30 PM",
     description: "This tour will explore the ways in which the Renaissance has influenced our own culture and society. You will see how Renaissance ideas about art, science, and philosophy have shaped our world today.",
     themeImage: "./img/latest-events/renaissance_and_you.webp"
@@ -18,7 +18,7 @@ const renaissanceTours = [
   {
     id: 3,
     title: "Renaissance Art and Music",
-    date: "2023-12-23",
+    date: "DEC 23, 2023",
     time: "5:30 PM – 8:15 PM",
     description: "This Christmas tour will explore the relationship between art and music in the Renaissance. You will see how artists and musicians of the time were inspired by each other and how their work influenced each other.",
     themeImage: "./img/latest-events/art_and_music.webp"
@@ -26,7 +26,7 @@ const renaissanceTours = [
   {
     id: 4,
     title: "Renaissance Women Artists",
-    date: "2024-05-12",
+    date: "MAY 12, 2024",
     time: "12:30 PM – 1:15 PM",
     description: "We'll focus on the work of women artists from the Renaissance period. You'll learn about the challenges that these women faced and how they overcame them to create the most beautiful artworks of the time.",
     themeImage: "./img/latest-events/women_artists.webp"
